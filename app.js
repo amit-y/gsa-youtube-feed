3e83a42899fe60c5899223185b87325237fdcb2c
Amit Yathirajadasan <amit@onceuponabreak.com>
2014-08-15 23:58:39 -0400

    create video pages for gsa to index

--- app.js
+++ app.js
@@ -24,25 +24,28 @@ appQ = Q(ytapiRequest(channelsListURL))
 
 .then(function() {
     //console.log(videos[0]);
-    return Q.nfcall(fs.readFile, 'feed_template.xml', 'utf-8');
+    //return Q.nfcall(fs.readFile, 'feed_template.xml', 'utf-8');
+    return Q.nfcall(fs.readFile, 'html_template.html', 'utf-8');
 })
 
 .then(function(feedsrc) {
-  var feed = handlebars.compile(feedsrc);
-  var data = {videos: videos};
-  return feed(data);
+  //var feed = handlebars.compile(feedsrc);
+  //var data = {videos: videos};
+  //return feed(data);
+  return pages(feedsrc);
 })
 
 .then(function(fd) {
   //console.log(fd);
-  return postFeed('http://posttestserver.com/post.php?dir=myfeedexample','test','test',new Buffer('test','utf-8'));
+  //return postFeed('http://posttestserver.com/post.php?dir=myfeedexample','test','test',new Buffer('test','utf-8'));
 })
 
 .then(function(postresp) {
-  console.log(postresp);
+  //console.log(postresp);
 })
 
 appQ.done(function() {
+  console.log(videos[0]);
   console.log('done!');
 })
 
@@ -51,6 +54,43 @@ appQ.done(function() {
 appQ.fail(function (err) {
   console.log(err);
 });
+
+function loopCreatePage(src, v) {
+  var deferred = Q.defer();
+  var vf = videos[v].id;
+  var page = createPage(videos[v], src).then(function(videoPage) {
+    return Q.nfcall(fs.writeFile, 'videos/'+vf+'.html', videoPage, 'utf-8')
+  })
+  page.done(function() {
+    deferred.resolve();
+  })
+  
+  //Q.nextTick(loop);
+  return deferred.promise;
+}
+
+function pages(src) {
+    var deferred = Q.defer();
+    var v=-1;
+    noMoreItems = false;
+
+    function loop() {
+        if (noMoreItems) return deferred.resolve();
+        v++;
+        Q.when(loopCreatePage(src, v), loop, deferred.reject);
+    }
+
+    Q.nextTick(loop);
+    return deferred.promise;
+}
+
+function createPage(video, src) {
+  var deferred = Q.defer();
+  var html = handlebars.compile(src);
+  var data = {id: video.id, title: video.title, description: video.description, bite: video.bite, thumbnails: video.thumbnails, videos: videos};
+  deferred.resolve(html(data));
+  return deferred.promise;
+}
 
 function postFeed(url,datasource,feedtype,data) {
   return Q.Promise(function(resolve, reject) {
